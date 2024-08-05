942ae19b8a0882d34fdca332c3ee9a1442bce07d
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-08-06 00:00:34 +0500

    "Updated CSS and JSX files in various components, including Footer, Navbar, TitleCards, Home, and Player, with changes to styles, layouts, and functionality."

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
+import Player from "./pages/Player/Player";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/player/:id" element={<Player />} />
+      </Routes>
     </>
   );
 }
