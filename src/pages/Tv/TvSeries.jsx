991a3b12348426f9730f29de4fe9e5a6bd3656e3
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-08-07 01:52:12 +0500

    "Updated CSS and JSX files for Navbar and TvSeries components, including changes to styles, layout, and HTML structure."

--- src/pages/Tv/TvSeries.jsx
+++ src/pages/Tv/TvSeries.jsx
@@ -9,12 +9,11 @@ const TvSeries = () => {
   const [hasMore, setHasMore] = useState(true);
 
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTVkMzFmMGU1NDk1OWUyOTdmM2RkYTFmMjc0OGZjOCIsIm5iZiI6MTcyMjk1ODc5MS42MzM5NDEsInN1YiI6IjY2YWQ0MTEyMjM1Y2M2NmQyZDY0YWYyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rrAGX5E6YzOEio8-0e3xG2-k0nZ5GPh7XL34zpM-ZzQ",
-    },
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTVkMzFmMGU1NDk1OWUyOTdmM2RkYTFmMjc0OGZjOCIsIm5iZiI6MTcyMjk1ODc5MS42MzM5NDEsInN1YiI6IjY2YWQ0MTEyMjM1Y2M2NmQyZDY0YWYyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rrAGX5E6YzOEio8-0e3xG2-k0nZ5GPh7XL34zpM-ZzQ'
+    }
   };
 
   useEffect(() => {
@@ -24,7 +23,7 @@ const TvSeries = () => {
   const fetchItems = async () => {
     try {
       const response = await fetch(
-        `https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=${page}`,
+        `https://api.themoviedb.org/3/tv/popular?language=en-US&page=${page}`,
         options
       );
       const result = await response.json();
@@ -40,29 +39,48 @@ const TvSeries = () => {
   };
 
   return (
-    <div className="container-fluid">
+    <>
       <Navbar />
-      <div className="row">
-        <InfiniteScroll
-          dataLength={data.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          endMessage={<p>No more items</p>}
-        >
-          {data.map((item, index) => (
-            <div key={index}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                alt={item.original_name}
-              />
-              <h1>{item.original_name}</h1>
-            </div>
-          ))}
-        </InfiniteScroll>
-      </div>
-    </div>
+      <section className="text-gray-600d body-fontd">
+        <div className="containerd">
+          <div className="gridd">
+            <InfiniteScroll
+              dataLength={data.length}
+              next={fetchMoreData}
+              hasMore={hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={<p>No more items</p>}
+            >
+              <div className="card-containerd">
+                {data.map((item, index) => (
+                  <div className="cardd" key={index}>
+                    <div className="card-contentd">
+                      <img
+                        className="imaged"
+                        src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
+                        alt={item.original_name}
+                      />
+                      <div className="text-contentd">
+                        <h1 className="titled">{item.original_name}</h1>
+                        <p className="descriptiond">{item.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </InfiniteScroll>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
+// <div key={index}>
+//   <img
+//     src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+//     alt={item.original_name}
+//   />
+//   <h1>{item.original_name}</h1>
+// </div>
 
 export default TvSeries;
